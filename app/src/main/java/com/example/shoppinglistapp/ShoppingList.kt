c398f4a20538164907e7dc954b89bcf294cd9840
GFO <gia.focxverashvili@gmail.com>
2023-12-25 09:51:08 +0400

    add item

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.example.shoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
@@ -58,8 +62,7 @@ fun ShoppingListApp() {
                 .padding(16.dp)
         ) {
             items(sItems) {
-
-
+               ShoppingListItem(it,  {}, {})
             }
         }
     }
@@ -123,4 +126,29 @@ fun ShoppingListApp() {
 
 
     }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+    ){
+
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize()
+            .border(
+                border = BorderStroke(2.dp, Color(0XFF018786)),
+                shape = RoundedCornerShape(20)
+            )
+    ) {
+
+        Text(text = item.name, modifier =Modifier.padding(8.dp))
+
+
+    }
+
+
 }
